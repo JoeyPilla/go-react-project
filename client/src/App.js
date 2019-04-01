722b50280587b66680570c5d07b127cd13af0b24
JoeyPilla <Pilla.4@osu.edu>
2019-03-31 19:59:02 -0500

    added server talking to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  componentDidMount() {
+export default function App() {
+  const [res, setRes] = useState('');
+  useEffect(() => {
     fetch('http://localhost:3000/hello')
-    .then(function(response) {
-      // The response is a Response instance.
-      // You parse the data into a useable format using `.json()`
-      return response.text();
-    }).then(function(data) {
-      // `data` is the parsed version of the JSON returned from the above endpoint.
-      console.log(data);  // { "userId": 1, "id": 1, "title": "...", "body": "..." }
-    });
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+      .then(function (response) {
+        response.text()
+          .then(resp => {
+            setRes(resp)
+        })
+      });
+  }, []);
+  return (
+    <div className="App">
+      <h1>{res}</h1>
+    </div>
+  );
 }
-
-export default App;
--- server/main.go
+++ server/main.go
@@ -10,7 +10,7 @@ func main() {
 	fs := http.FileServer(http.Dir("../client/build"))
 	http.Handle("/", fs)
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website! %s", r.URL.Path)
+		fmt.Fprintf(w, "Hello from server!")
 	})
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
